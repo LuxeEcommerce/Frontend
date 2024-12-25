afd0b1f6a4806a120a62bfa4f47ee633a6f8e1e3
miurayuuki05 <lolipopfaiz0@gmail.com>
2024-12-25 12:58:23 +0700

    add image

--- app/components/Bannerads.tsx
+++ app/components/Bannerads.tsx
@@ -15,11 +15,11 @@ const Bannerads: React.FC<BanneradsProps> = ({split = 1, titleList, descList}) =
             <div className="flex justify-between px-20">
                 {split === 1 ? (
                     <div className="w-[70%] h-[400px] pt-10">
-                        <Image src="/placehold.svg" className="object-cover h-[350px]" alt="banner1" width={800} height={400} />
+                        <Image src="/promo/whitepromo.png" className="object-cover h-[350px]" alt="banner1" width={800} height={400} />
                     </div>
                 ) : (
                     <div className="w-[45%] h-[400px] pt-10">
-                        <Image src="/placehold.svg" className="object-cover h-[350px]" alt="banner1" width={800} height={400} />
+                        <Image src="/promo/blackfriday1.png" className="object-cover object-left h-[350px]" alt="banner1" width={800} height={400} />
                     </div>
                 )}
                 {split === 1 ? (
@@ -37,7 +37,7 @@ const Bannerads: React.FC<BanneradsProps> = ({split = 1, titleList, descList}) =
             {split === 2 && (
                 <div className="flex justify-between px-20 mt-10 mb-20">
                     <div className="w-[45%] h-[400px] pt-10">
-                        <Image src="/placehold.svg" className="object-cover h-[350px]" alt="banner2" width={800} height={400} />
+                        <Image src="/promo/blackfriday2.png" className="object-cover object-right h-[350px]" alt="banner2" width={800} height={400} />
                     </div>
                     <div className="border-l border-gray-300 pt-10 pl-20">
                         <h1 className="text-3xl font-bold">{titleList[1]}</h1>
--- app/order/invoice/page.tsx
+++ app/order/invoice/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import Image from 'next/image';
 
 export default function About() {
   return (
